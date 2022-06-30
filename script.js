5733c21e1ccf2e14935e9e8e55f74aafb58390b9
naijatechgirle <ruthiekelly38@gmail.com>
2022-06-30 11:50:20 +0100

    dynamically adding html using javascript and populating cards using an api

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-fetch('https://fakestoreapi.com/products')
-    .then((data) => {
-        console.log(data);
-    let data1 = data.json();
-    console.log(data1);
-}).catch((error) => {
-    console.log(error);
-})
+fetch('https://fakestoreapi.com/products').then((data1) => {
+    return data1.json();
+}).then((completedata) => {
+    let data = '';
+    completedata.map((values) => {
+        data += `<div class="storeCards">
+        <h3 class="sub-title">${values.title}</h3>
+        <img src="${values.image}" class="images" alt="product-images">
+        <p class="description">${values.description}}</p>
+        <h6 class="price">$ ${values.price}</h6>
+    </div>`
+    });
+    document.getElementById('container').innerHTML = data;
+}).catch((err) => {
+    conosole.log(err);
+});
